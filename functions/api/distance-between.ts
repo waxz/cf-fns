49c421e85a5f10867b8d21a26546aaba787d5b7a
waxz <waxz@github.com>
2025-04-30 13:35:36 +0000

    fix wasm load process, run ok ;

--- functions/api/distance-between.ts
+++ functions/api/distance-between.ts
@@ -1,12 +1,35 @@
+import * as wasm from "../../app-rust/pkg/src_wasm_bg.js";
+// @ts-ignore
 import wasmModule from "../../app-rust/pkg/src_wasm_bg.wasm";
+
 import { GeoCoordinates } from "../../app-web/src/globe/geoCoordinates.interface";
 
 export async function onRequestPost({ request }): Promise<Response> {
-  const { from, to } = await request.json() as {from: GeoCoordinates, to: GeoCoordinates};
+  const { from, to } = await request.json() as { from: GeoCoordinates; to: GeoCoordinates };
 
-  const wasmModuleInstance = await WebAssembly.instantiate(wasmModule);
-  const distanceBetweenFn = wasmModuleInstance.exports.distance_between as CallableFunction;
-  const distanceBetween = distanceBetweenFn(from.latitude, from.longitude, to.latitude, to.longitude);
+  try {
+    const imports = {
+      "./src_wasm_bg.js": wasm,
+    };
 
-	return new Response(distanceBetween);
+    const result = await WebAssembly.instantiate(wasmModule, imports);
+
+    if (!result) {
+      console.error("WebAssembly instantiation failed (no result):", result);
+      return new Response("Error instantiating WebAssembly", { status: 500 });
+    }
+
+    wasm.__wbg_set_wasm(result.exports); // Try accessing exports directly from 'result'
+
+    const distanceBetween = wasm.distance_between(
+      from.latitude,
+      from.longitude,
+      to.latitude,
+      to.longitude
+    );
+    return new Response(distanceBetween.toString());
+  } catch (error) {
+    console.error("Error using WebAssembly:", error);
+    return new Response("Error calculating distance", { status: 500 });
+  }
 }

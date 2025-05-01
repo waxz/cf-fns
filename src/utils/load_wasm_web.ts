b5810e4442d0aea102e7c60c41b8e30c001b5acf
waxz <waxz@github.com>
2025-05-01 08:58:38 +0000

    clean code

--- src/utils/load_wasm_web.ts
+++ src/utils/load_wasm_web.ts
@@ -1,22 +1,22 @@
-import init   from "../../app-rust/pkg/web/src_wasm.js";
+import init from "../../app-rust/pkg/web/src_wasm.js";
 
-  import wasmUrl from "../../app-rust/pkg/web/src_wasm_bg.wasm"; // 👈 this is crucial
-  import wasmBinary from '../../app-rust/pkg/web/src_wasm_bg.wasm?module';
+import wasmUrl from "../../app-rust/pkg/web/src_wasm_bg.wasm"; // 👈 this is crucial
+import wasmBinary from '../../app-rust/pkg/web/src_wasm_bg.wasm?module';
 
-  let wasmInstance: WebAssembly.WebAssemblyInstantiatedSource;
+let wasmInstance: WebAssembly.WebAssemblyInstantiatedSource;
 
 // @ts-ignore
 import wasmModule from "../../app-rust/pkg/src_wasm_bg.wasm";
 let wasmReady = false;
 let wasm;
 
-export async function load_wasm(context, f){
-    const {request, env} = context;
+export async function load_wasm(context, f) {
+    const { request, env } = context;
 
     var url = new URL(request.url);
     url.pathname = "/pkg/web/src_wasm_bg.wasm";
     var newReq = new Request(
-      url.toString()
+        url.toString()
     );
 
 
@@ -26,39 +26,41 @@ export async function load_wasm(context, f){
 
             {
                 // run ok
-            wasm = await init(wasmUrl);
+                //✘ [ERROR] using deprecated parameters for the initialization function; pass a single object instead
+                wasm = await init(wasmUrl);
 
             }
+        
 
-            { 
-                // const wasmBinary = await env.ASSETS.fetch(newReq);
-                // const buffer = await wasmBinary.arrayBuffer();
-                // await init(buffer); // or: 
-                //  await init({ module: buffer });
+            {
+                //Error using WebAssembly: CompileError: WebAssembly.instantiate(): Wasm code generation disallowed by embedder
+                // const response = await env.ASSETS.fetch(newReq);
+                // const buffer = await response.arrayBuffer();  // <-- fix here
+                // wasm = await WebAssembly.instantiate(buffer, {});
             }
- 
- 
-
-          }
-
-          const ptr = wasm.get_char_buffer_ptr();
-          const len = wasm.get_char_buffer_len();
-          const mem = new Uint8Array(wasm.memory.buffer, ptr, len);
-  mem[0] = 5;
-  mem[1] = 7;
-
-  const sum = wasm.print_buffer();
-  console.log(`sum = ${sum}`)
-
- 
-
-  return f(wasm);
 
 
-     
-      } catch (error) {
+
+        }
+
+        // const ptr = wasm.get_char_buffer_ptr();
+        // const len = wasm.get_char_buffer_len();
+        // const mem = new Uint8Array(wasm.memory.buffer, ptr, len);
+        // mem[0] = 5;
+        // mem[1] = 7;
+
+        // const sum = wasm.print_buffer();
+        // console.log(`sum = ${sum}`)
+
+
+
+        return f(wasm);
+
+
+
+    } catch (error) {
         console.error("Error using WebAssembly:", error);
 
-      }
+    }
 
 }

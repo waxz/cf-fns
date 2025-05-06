442a1955ceab3afb44d2758e6019d12c437cc4b0
waxz <waxz@github.com>
2025-05-06 16:21:37 +0000

    add burn-vision test

--- wasi-test-rust/src/bin/main.rs
+++ wasi-test-rust/src/bin/main.rs
@@ -10,13 +10,36 @@ use std::io::{self, BufRead};
 
 // Use this example with something like mitmproxy
 // $ mitmproxy --listen-port 8080
-use burn::tensor;
+use burn::{
+    tensor::Tensor
+};
 // use burn::backend::NdArray;
 
 // Type alias for the backend to use.
 // type Backend = NdArray;
 
+use burn::nn;
 
+use burn::module::Module;
+use burn::tensor::backend::Backend;
+
+#[derive(Module, Debug)]
+pub struct PositionWiseFeedForward<B: Backend> {
+    linear_inner: nn::Linear<B>,
+    linear_outer: nn::Linear<B>,
+    dropout: nn::Dropout,
+    gelu: nn::Gelu,
+}
+
+impl<B: Backend> PositionWiseFeedForward<B> {
+    pub fn forward<const D: usize>(&self, input: Tensor<B, D>) -> Tensor<B, D> {
+        let x = self.linear_inner.forward(input);
+        let x = self.gelu.forward(x);
+        let x = self.dropout.forward(x);
+
+        self.linear_outer.forward(x)
+    }
+}
 fn run_burn(){
 
 }

812cd3f2d1e4693c03e6e0c186ec813c5c978c88
waxz <waxz@github.com>
2025-05-10 05:09:15 +0000

    add wasm-opt alias

---
+++ wasi-rust/build-wasm-opt.sh
@@ -0,0 +1,6 @@
+
+if [ -z "$1" ]; then
+echo please set target_dir;
+fi;
+
+find $1 -type f -name "*wasm" -print0 | while IFS= read -r -d $'\0' file; do   filename=$(basename "$file");   extension="${filename##*.}";   directory=$(dirname "$file");   echo "File: $file";   echo "Filename: $filename";   echo "Extension: $extension";   echo "Directory: $directory";   if [[ ! "$filename" == *"async_"* ]]; then wasm-opt $directory/$filename --asyncify -O3 -o $directory/async_$filename;fi ; done

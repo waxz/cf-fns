033bed2fcb1ceca887add2f1f4cc35c925f1d892
waxz <waxz@github.com>
2025-05-11 08:16:28 +0000

    add build script

--- wasi-rust/build-wasm-opt.sh
+++ wasi-rust/build-wasm-opt.sh
@@ -2,5 +2,7 @@
 if [ -z "$1" ]; then
 echo please set target_dir;
 fi;
-
+if [ ! -d "$1" ]; then
+    echo  target_dir $1 not found;
+    fi;
 find $1 -type f -name "*wasm" -print0 | while IFS= read -r -d $'\0' file; do   filename=$(basename "$file");   extension="${filename##*.}";   directory=$(dirname "$file");   echo "File: $file";   echo "Filename: $filename";   echo "Extension: $extension";   echo "Directory: $directory";   if [[ ! "$filename" == *"async_"* ]]; then wasm-opt $directory/$filename --asyncify -O3 -o $directory/async_$filename;fi ; done
